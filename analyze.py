a7c0ea026454fd41c8dc06307d97b16f1a5fe811
Matt Jarvis <matt.jarvis@astinc.us>
2015-03-23 19:48:18 -0400

    Method testing

---
+++ analyze.py
@@ -0,0 +1,129 @@
+data = """
+Exhaustive Grid Search	0	357.2510002	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	1	354.1949999	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	2	350.1259999	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	3	323.3740001	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	4	264.3150001	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	5	264.9219999	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	6	263.102	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	7	268.566	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	8	267.398	47.19242892	40.72261241	6.469816511
+Exhaustive Grid Search	9	261.184	47.19242892	40.72261241	6.469816511
+Partial Grid Search	0	27.54299998	47.19242892	40.68510842	6.507320506
+Partial Grid Search	1	26.921	47.19242892	40.68510842	6.507320506
+Partial Grid Search	2	26.86800003	47.19242892	40.68510842	6.507320506
+Partial Grid Search	3	27.7019999	47.19242892	40.68510842	6.507320506
+Partial Grid Search	4	29.63499999	47.19242892	40.68510842	6.507320506
+Partial Grid Search	5	28.01600003	47.19242892	40.68510842	6.507320506
+Partial Grid Search	6	27.18999982	47.19242892	40.68510842	6.507320506
+Partial Grid Search	7	29.579	47.19242892	40.68510842	6.507320506
+Partial Grid Search	8	28.20900011	47.19242892	40.68510842	6.507320506
+Partial Grid Search	9	27.81700015	47.19242892	40.68510842	6.507320506
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	0	11.98599982	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	1	11.41000009	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	2	11.50100017	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	3	11.81599998	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	4	11.82799983	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	5	12.47000003	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	6	11.898	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	7	11.95899987	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	8	11.71700001	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Sparse Grid Restart	9	11.90999985	47.19242892	40.65692552	6.535503404
+Nelder-Mead Downhill Simplex, Dense Grid Restart	0	43.27600002	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	1	41.53600001	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	2	44.01400018	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	3	41.29999995	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	4	41.86800003	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	5	43.87300015	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	6	41.34800005	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	7	42.48000002	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	8	42.93300009	47.19242892	40.46793976	6.724489166
+Nelder-Mead Downhill Simplex, Dense Grid Restart	9	41.59200001	47.19242892	40.46793976	6.724489166
+Basin hopping with Nelder Mead	0	12.18700004	47.19242892	41.26611159	5.926317329
+Basin hopping with Nelder Mead	1	12.99500012	47.19242892	40.46793986	6.724489065
+Basin hopping with Nelder Mead	2	12.21800017	47.19242892	40.4679396	6.724489328
+Basin hopping with Nelder Mead	3	12.09599996	47.19242892	40.46793979	6.724489136
+Basin hopping with Nelder Mead	4	13.24699998	47.19242892	40.4679397	6.724489224
+Basin hopping with Nelder Mead	5	12.91999984	47.19242892	40.46794	6.72448892
+Basin hopping with Nelder Mead	6	13.42000008	47.19242892	40.46794265	6.724486276
+Basin hopping with Nelder Mead	7	12.48500013	47.19242892	40.46793979	6.724489134
+Basin hopping with Nelder Mead	8	11.90100002	47.19242892	40.46798572	6.7244432
+Basin hopping with Nelder Mead	9	12.43199992	47.19242892	40.46793973	6.724489197
+Basin hopping with Nelder Mead	10	12.81900001	47.19242892	40.46793975	6.724489177
+Basin hopping with Nelder Mead	11	13.04399991	47.19242892	40.46793962	6.724489301
+Basin hopping with Nelder Mead	12	13.15700006	47.19242892	40.46793976	6.724489162
+Basin hopping with Nelder Mead	13	12.47600007	47.19242892	40.46794163	6.724487293
+Basin hopping with Nelder Mead	14	12.57299995	47.19242892	40.46793959	6.724489332
+Basin hopping with Nelder Mead	15	12.92799997	47.19242892	40.46793959	6.724489329
+Basin hopping with Nelder Mead	16	11.7980001	47.19242892	40.46794002	6.724488899
+Basin hopping with Nelder Mead	17	12.51399994	47.19242892	40.53732405	6.655104878
+Basin hopping with Nelder Mead	18	14.05500007	47.19242892	40.46793992	6.724489
+Basin hopping with Nelder Mead	19	12.92499995	47.19242892	40.46793979	6.724489132
+Basin hopping with Nelder Mead	20	12.99900007	47.19242892	40.46793981	6.72448911
+Basin hopping with Nelder Mead	21	12.72900009	47.19242892	40.46793962	6.724489307
+Basin hopping with Nelder Mead	22	12.98000002	47.19242892	40.46793976	6.724489168
+Basin hopping with Nelder Mead	23	12.33500004	47.19242892	40.46794019	6.724488731
+Basin hopping with Nelder Mead	24	12.92400002	47.19242892	40.46793987	6.724489049
+Basin hopping with Nelder Mead	25	12.36199999	47.19242892	40.65692566	6.535503261
+Basin hopping with Nelder Mead	26	12.70599985	47.19242892	40.46794056	6.724488362
+Basin hopping with Nelder Mead	27	12.26800013	47.19242892	40.46794109	6.724487837
+Basin hopping with Nelder Mead	28	11.7349999	47.19242892	41.17841633	6.014012598
+Basin hopping with Nelder Mead	29	14.14400005	47.19242892	40.46794014	6.724488785
+Basin hopping with Nelder Mead	30	13.37699986	47.19242892	40.46807867	6.724350251
+Basin hopping with Nelder Mead	31	13.11100006	47.19242892	40.46794065	6.724488273
+Basin hopping with Nelder Mead	32	13.09099984	47.19242892	40.46793979	6.724489137
+Basin hopping with Nelder Mead	33	13.55800009	47.19242892	40.47060508	6.721823841
+Basin hopping with Nelder Mead	34	13.1170001	47.19242892	41.30639428	5.886034644
+Basin hopping with Nelder Mead	35	12.65100002	47.19242892	40.46793973	6.724489198
+Basin hopping with Nelder Mead	36	12.79400015	47.19242892	40.46793976	6.724489166
+Basin hopping with Nelder Mead	37	12.40199995	47.19242892	40.46793982	6.724489106
+Basin hopping with Nelder Mead	38	13.46799994	47.19242892	40.46793973	6.724489194
+Basin hopping with Nelder Mead	39	12.5710001	47.19242892	40.65692557	6.53550335
+Basin hopping with Nelder Mead	40	12.46600008	47.19242892	40.46793964	6.724489281
+Basin hopping with Nelder Mead	41	12.87399983	47.19242892	41.17841636	6.014012561
+Basin hopping with Nelder Mead	42	13.12400007	47.19242892	40.46793978	6.724489139
+Basin hopping with Nelder Mead	43	13.26300001	47.19242892	40.46794016	6.724488767
+Basin hopping with Nelder Mead	44	13.01999998	47.19242892	40.46794051	6.724488416
+Basin hopping with Nelder Mead	45	12.86999989	47.19242892	40.46793962	6.724489304
+Basin hopping with Nelder Mead	46	13.89700007	47.19242892	40.46793982	6.724489106
+Basin hopping with Nelder Mead	47	12.36699986	47.19242892	40.46793964	6.724489287
+Basin hopping with Nelder Mead	48	12.91400003	47.19242892	40.46793965	6.724489272
+Basin hopping with Nelder Mead	49	12.58699989	47.19242892	40.46793983	6.724489089
+"""
+
+
+import math
+
+data = [line.strip().split("\t") for line in data.split("\n") if line.strip()]
+
+trials = []
+
+for line in data:
+	method, trial, time, initial, final, cost = line
+	trials.append({"method":method, "trial":int(trial), "time":float(time), "initial":float(initial), "final":float(final), "cost":float(cost)})
+
+# Get all method names
+names = list(set([x['method'] for x in trials]))
+
+# Get optimum cost
+optimum_cost = max([x['cost'] for x in trials])
+
+
+for method in names:
+
+	sub_trials = [x for x in trials if x['method'] == method]
+
+	# Get average time
+	average_time = sum([x['time'] for x in sub_trials]) / len(sub_trials)
+
+	# Get average result
+	average_cost = sum([x['cost'] for x in sub_trials]) / len(sub_trials)
+	max_cost = max([x['cost'] for x in sub_trials])
+
+	# Get percentage within 5 decimals of average result
+	meets_tol = len([x['cost'] for x in sub_trials if abs(x['cost'] - max_cost) < 0.00001]) / float(len(sub_trials))
+
+	# Get the difference from optimal
+	diff = optimum_cost - max_cost
+
+	print "{}\t{}\t{}\t{}\t{}".format(method, average_time, average_cost, meets_tol, diff)
--- query_test.py
+++ query_test.py
@@ -2,7 +2,7 @@
 import library.tests
 import library.costs
 import plot_manifold
-
+import time
 import datetime
 
 def main():
@@ -27,9 +27,9 @@ def main():
                 ]
 
     # Plot the manifold and the two point sets
-    plot_manifold.plot(tests, "Tolerance Cost Shift", point_sets)
-
-    # And other stuff
+    # plot_manifold.plot(tests, "Tolerance Cost Shift", point_sets)
+    print tolerance_results
+    # And other stuff`
 
 def main2():
     tests = library.tests.TestLibrary("data")
@@ -43,5 +43,35 @@ def main2():
 
     plot_manifold.plot(tests, "Covariation Cost Shift", point_sets)
 
+
+def main3():
+
+    # Load a test group of just Jarrad's tests
+    tests = library.tests.TestLibrary("data")
+    tests = tests.filter({"subject": "Jarrad"})
+    print "starting"
+    for i in range(50):
+        start = time.time()
+        # Compute the tolerance cost and capture the results
+        tolerance_results = library.costs.compute_tolerance_cost(tests)
+        output =  "trial {}, {} seconds, initial = {}, final = {}, result = {}".format(i, time.time() - start, tolerance_results['initial_score'], tolerance_results['final_score'], tolerance_results['cost'])
+
+        with open("sternad2009.txt", "a") as handle:
+            handle.write(output)
+            handle.write("\n")
+
+        print output 
+    print "done"
+
+def main4():
+	# Load a test group of just Jarrad's tests
+    tests = library.tests.TestLibrary("data")
+    tests = tests.filter({"subject": "Jarrad"})
+
+    start = time.time()
+    table = library.costs.compute_tolerance_cost(tests)
+
+    print "{} s".format(time.time() - start)
+
 if __name__ == "__main__":
-    main2()
+    main3()
