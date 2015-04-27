abf15c8cac029a385e7b697e2263e15669407280
Matt Jarvis <mattj23@gmail.com>
2015-04-26 20:54:40 -0400

    Detrended Fluctuation Analysis using dfa.c seems to be working, demo_query_temporal.py created

--- library/temporal.py
+++ library/temporal.py
@@ -6,6 +6,7 @@ __author__ = 'matt'
 
 import tests
 import numpy
+import scipy.stats
 import subprocess
 import os
 
@@ -60,4 +61,38 @@ def detrended_fluctuation_analysis(test_group, property):
     if not os.path.exists(executable):
         raise Exception("Couldn't find the dfa.exe executable")
 
-    print "all is well"
+    # From the instructions, the input data should be a single column
+    # of numbers in text format.  The output will be two columns of
+    # numbers, which are the base 10 logarithms of n and F(n), respectively.
+    # The dfa.c program does not compute a scaling exponent, so to do so
+    # we must fit the output to a line and measure its slope.
+
+    # Extract the value of interest from the test group
+    property_values, filenames = test_group.get_list_of_key(property)
+
+    # Prepare it for the standard input
+    std_input = "\n".join(["{}".format(x) for x in property_values])
+
+    # Create the subprocess and read the results
+    process = subprocess.Popen(executable,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate(std_input)
+
+    # Process the standard out
+    result = [[float(y) for y in x.strip().split()] for x in stdout.split("\n") if x.strip()]
+    x_values, y_values = zip(*result)
+
+    slope, intercept, r_value, p_value, standard_error = scipy.stats.linregress(x_values, y_values)
+
+    output_data = {"dfa_outputs": {"log10_n": x_values, "log10_f(n)": y_values},
+                   "linear_regression": {"slope": slope,
+                                         "intercept": intercept,
+                                         "r_value": r_value,
+                                         "p_value": p_value,
+                                         "stderr": standard_error}}
+
+    return output_data
+
+
